5411409d0deabbf4f119e2830bc304b16dea86cf
handy <handy@ahrefs.com>
2015-04-27 12:15:43 +0800

    Handle curl_multi_select() infinite loop on curl roughly version > 7.19

--- lib/AhrefsAPI.php
+++ lib/AhrefsAPI.php
@@ -397,11 +397,12 @@ class AhrefsAPI {
         } while ($mrc == CURLM_CALL_MULTI_PERFORM);
 
         while ($active && $mrc == CURLM_OK) {
-            if (curl_multi_select($mh) != -1) {
-                do {
-                    $mrc = curl_multi_exec($mh, $active);
-                } while ($mrc == CURLM_CALL_MULTI_PERFORM);
+            if (curl_multi_select($mh) == -1) {
+                usleep(100);
             }
+            do {
+                $mrc = curl_multi_exec($mh, $active);
+            } while ($mrc == CURLM_CALL_MULTI_PERFORM);
         }
 
         $this->curlInfo = array();
